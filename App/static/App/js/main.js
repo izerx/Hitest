100808e7bb1474063ce3cb226674638d6311e947
Roman <romshtmb@gmail.com>
2022-02-16 17:54:09 +0300

    v1.0.1

--- App/static/App/js/main.js
+++ App/static/App/js/main.js
@@ -40,34 +40,34 @@ $('#add_question_modal').on('show.bs.modal', function (event) {
   modal.find('.modal-footer button#add').attr('onclick', 'add_question('+subject_id+')')
 })
 
-function add_question(subject_id){
-  $.ajax({
-    url: '/',
-    type: 'post',
-    data: {
-      'action': 'add_question',
-      'subject_id': subject_id,
-      'text': $([name=q_text]).val(),
-      'answer': $([name=q_answer]).val(),
-      'q_dop_1': $([name=q_dop_1]).val(),
-      'q_dop_2': $([name=q_dop_2]).val(),
-      'q_dop_3': $([name=q_dop_3]).val()
-    },
-    success: function(responce){
-      $('#add_question_modal').modal('hide');
-      $([name=q_text]).val('');
-      $([name=q_answer]).val('');
-      $([name=q_dop_1]).val('');
-      $([name=q_dop_2]).val('');
-      $([name=q_dop_3]).val('');
-    },
-    error: function(responce){
-      console.log(2);
-    }
-  })
-}
+// function add_question(subject_id){
+//   $.ajax({
+//     url: '/',
+//     type: 'post',
+//     data: {
+//       'action': 'add_question',
+//       'subject_id': subject_id,
+//       'text': $([name=q_text]).val(),
+//       'answer': $([name=q_answer]).val(),
+//       'q_dop_1': $([name=q_dop_1]).val(),
+//       'q_dop_2': $([name=q_dop_2]).val(),
+//       'q_dop_3': $([name=q_dop_3]).val()
+//     },
+//     success: function(responce){
+//       $('#add_question_modal').modal('hide');
+//       $([name=q_text]).val('');
+//       $([name=q_answer]).val('');
+//       $([name=q_dop_1]).val('');
+//       $([name=q_dop_2]).val('');
+//       $([name=q_dop_3]).val('');
+//     },
+//     error: function(responce){
+//       console.log(2);
+//     }
+//   })
+// }
 
-function add_question_1(subject_id){
+function add_question(subject_id){
   $.ajax({
     url: '/questions/'+subject_id,
     type: 'post',
